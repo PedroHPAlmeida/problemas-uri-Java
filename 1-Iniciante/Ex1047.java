a30d12335e44014dd6b10a16e51a02916e627f2d
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-01-28 16:45:14 -0300

    Tempo de Jogo com Minutos

--- 1-Iniciante/Ex1047.java
+++ 1-Iniciante/Ex1047.java
@@ -20,13 +20,13 @@ public class Ex1047 { // para ser aceito no Beecrowd mude o nome da classe para 
         int tempoEmMinutosInicio = hrInicio * 60 + minInicio; // calcula o tempo em minutos no momento de inicio do jogo
         int tempoEmMinutosFim = hrFim * 60 + minFim; // calcula o tempo em minutos no momento em que o jogo eh encerrado
 
-        int duracaoHoras = tempoEmMinutosInicio >= tempoEmMinutosFim
-                ? ((24 * 60 - (tempoEmMinutosInicio - tempoEmMinutosFim)) / 60)
-                : ((tempoEmMinutosFim - tempoEmMinutosInicio) / 60);  // se o tempoEmMinutosInicio for >=  a tempoEmMinutosFim significa que o jogo terminou apenas no dia seguinte, entao eh o feito o calulo apropriado. Caso contrario eh feito um calculo mais simples para verificar a duracao do jogo.
+        int duracaoHoras = tempoEmMinutosInicio < tempoEmMinutosFim
+                ? ((tempoEmMinutosFim - tempoEmMinutosInicio) / 60)
+                : ((24 * 60 - (tempoEmMinutosInicio - tempoEmMinutosFim)) / 60);  // se o tempoEmMinutosInicio for < que tempoEmMinutosFim significa que o jogo terminou no mesmo dia, entao eh o feito o calulo apropriado. Caso contrario eh feito um calculo mais complexo para verificar a duracao do jogo.
 
-        int duracaoMinutos = tempoEmMinutosInicio >= tempoEmMinutosFim
-                ? ((24 * 60 - (tempoEmMinutosInicio - tempoEmMinutosFim)) % 60)
-                : ((tempoEmMinutosFim - tempoEmMinutosInicio) % 60); // mesma logica da variavel duracaoHoras
+        int duracaoMinutos = tempoEmMinutosInicio < tempoEmMinutosFim
+                ? ((tempoEmMinutosFim - tempoEmMinutosInicio) % 60)
+                : ((24 * 60 - (tempoEmMinutosInicio - tempoEmMinutosFim)) % 60); // mesma logica da variavel duracaoHoras
 
         // saida
         System.out.printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n", duracaoHoras, duracaoMinutos);
